8b9990d27548f92f5104ec35a33ec9a2aa7d78fb
amitMehraFullstack <amit.mehra@codebrewinnovations.com>
2024-05-27 23:59:30 +0530

    remove commented code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { UsersModule } from './users/users.module';
-// import { AppDataSource } from 'shared-orm-library/dist/data-source';
-// import { User } from 'shared-orm-library/dist/entities/user.entity';
-// import { Settings } from 'shared-orm-library/dist/entities/settings.entity';
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       host: 'localhost',
-//       port: 5432,
-//       username: 'codebrew',
-//       password: 'root123',
-//       database: 'UserAssignment',
-//       entities: [User, Settings],
-//       synchronize: false,
-//     }),
-//     TypeOrmModule.forFeature([User]),
-//     UsersModule,
-//   ],
-// })
-// export class AppModule {}
-
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
